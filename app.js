b5bc72083391d30863e78d98d8fcdeda2f7be941
abhijeet gupta <abhijeetg40@gmail.com>
2020-06-23 13:02:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+var square = document.getElementById('square')
+
+var html = `<div class="square">1</div>`
+for(var i =0; i<5 ;i++){
+for (let j = 0; j <5 ; j++) {
+    
+    var node = document.createElement('button')
+    node.textContent = `${i}` + `${j}`
+    node.className = 'square'
+    square.appendChild(node)
+
+}
+var temp = document.createElement('br')
+square.appendChild(temp)
+var temp = document.createElement('br')
+square.appendChild(temp)
+}
+
+var box = document.querySelectorAll('.square').forEach(item =>{
+    item.addEventListener('click',event =>{
+        console.log(item.textContent)
+    })
+})
+
